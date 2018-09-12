e99fa6ad8a43abf51b8c747ebeae44e9c2e69227
Lewis Reid <lewis.reid148@gmail.com>
2018-09-12 14:35:22 +0100

    Loop Prints Each Line of CSV

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,41 @@
 package training.supportbank;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
-        System.out.println("Test!");
+        //Scanner scanner = new Scanner(System.in);
+        //while (scanner.hasNextLine()) {
+        //    String userInput = scanner.nextLine();
+        //}
+
+
+        //Importing csv file
+        List<String> transactionsList = null;
+        try {
+            transactionsList = Files.readAllLines(Paths.get("C:\\Work\\Training\\SupportBank\\Transactions2014.csv"));
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+        //
+
+
+        System.out.println(transactionsList.get(1));
+
+        //Loop to input value into Transaction instances
+
+        Integer listSize = transactionsList.size();
+        int counter = 1;
+
+
+        while (counter < listSize) {
+        System.out.println(transactionsList.get(counter));
+            counter = counter + 1;
+        }
+
     }
 }
