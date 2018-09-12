5369bc81a64ce359e74f37859faefa7f082becf2
Lewis Reid <lewis.reid148@gmail.com>
2018-09-12 15:38:17 +0100

    Change Transaction Variables to String
    To be converted later

--- src/main/java/training/supportbank/Transaction.java
+++ src/main/java/training/supportbank/Transaction.java
@@ -3,9 +3,10 @@ package training.supportbank;
 import java.time.LocalDate;
 
 public class Transaction {
-    public LocalDate date;
+    public String date;
     public String from = "";
     public String to = "";
-    public String narative = "";
-    public Double amount;
+    public String narrative = "";
+    public String amount;
+
 }
