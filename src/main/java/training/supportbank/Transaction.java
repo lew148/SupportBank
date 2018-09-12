33522a841718f5a44a3eff3962e48a558ff95ce3
Lewis Reid <lewis.reid148@gmail.com>
2018-09-12 14:34:34 +0100

    Transaction Class Creation

---
+++ src/main/java/training/supportbank/Transaction.java
@@ -0,0 +1,11 @@
+package training.supportbank;
+
+import java.time.LocalDate;
+
+public class Transaction {
+    public LocalDate date;
+    public String from = "";
+    public String to = "";
+    public String narative = "";
+    public Double amount;
+}
