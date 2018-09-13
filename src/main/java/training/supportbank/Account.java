5ebef920d1151b85774b4d93e5418958eb5b6607
Lewis Reid <lewis.reid148@gmail.com>
2018-09-13 16:10:06 +0100

    Added Monetary Format (Truncation of Large Doubles)

--- src/main/java/training/supportbank/Account.java
+++ src/main/java/training/supportbank/Account.java
@@ -1,5 +1,6 @@
 package training.supportbank;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,10 +13,11 @@ public class Account {
     //list of transactions for the specific account instance is created upon instance creation
     public List<Transaction> accountTransactions = new ArrayList<>();
 
+    DecimalFormat df = new DecimalFormat("###########0.00");
 
     // prints string giving name, amount owed and amount due of person's account
     public void printOweDue() {
-        String OweDue = this.name + " owes £" + this.amountOwed + " and is due £" + this.amountDue + "!";
+        String OweDue = this.name + " owes £" + df.format(this.amountOwed) + " and is due £" + df.format(this.amountDue) + "!";
         System.out.println(OweDue);
     }
 
