c28bde34c3e076ba9dbebff227771d36fa3fde38
Lewis Reid <lewis.reid148@gmail.com>
2018-09-13 12:22:19 +0100

    Added getProfile Method

--- src/main/java/training/supportbank/Account.java
+++ src/main/java/training/supportbank/Account.java
@@ -11,4 +11,11 @@ public class Account {
 
     //list of transactions for the specific account instance is created upon instance creation
     public List<Transaction> accountTransactions = new ArrayList<>();
+
+
+
+    public String getProfile() {
+        String profile = this.name + " owes £" + Double.toString(this.amountOwed) + " and is due £" + Double.toString(this.amountDue) + "!";
+        return profile;
+    }
 }
